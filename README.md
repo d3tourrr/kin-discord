3701043510344f2b1ed183328ae6232e333f7da5
d3tourrr <wrhoades94@gmail.com>
2024-10-02 09:18:13 -0700

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+# Kindroid-Discord Integration
+
+[Kindroid](https://kindroid.ai) is a platform that offers AI companions for human users to chat with. They have opened v1 of their [API](https://docs.kindroid.ai/api-documentation) which enables Kindroid chatting that occurs outside of the Kindroid app or website. This Discord bot allows you to invite a Kindroid to Discord to chat with people there.
+
+# Setup
+
+You need an instance of this Discord bot per Nomi you wish you invite to a Discord server, but you can invite the same Discord Bot/Nomi pair to as many servers as you'd like.
+
+1. Make a Discord Application and Bot
+   1. Go to the [Discord Developer Portal](https://discord.com/developers/applications)
+   1. Create a new application and then a bot under that application
+   1. Copy the bot's token
+   1. Add the bot to a server with the required permissions (at least "Read Messages" and "Send Messages")
+1. Clone this repo: `git clone https://github.com/d3tourrr/kin-discord.git`
+   1. After cloning the repo, change to the directory: `cd kin-discord`
+1. Build the Docker image: `docker build -t kin-discord .`
+   1. Install Docker if you haven't already got it: [Instructions](https://docs.docker.com/engine/install/)
+1. Get your Kindroid API token
+   1. Open the side bar while chatting with a Kindroid and click General, then scroll to the bottom and expand API & advanced integration
+   1. Copy your API key
+1. Get the Nomi ID from the same place you copied your API key - note, you have to be chatting with the specific Kindroid who you wish to bring to Discord
+1. Run the Docker container: `docker run -e DISCORD_BOT_TOKEN=$DISCORD_BOT_TOKEN -e KIN_TOKEN=$KIN_TOKEN -e KIN_ID=$KIN_ID kin-discord`
+   1. Replace `$DISCORD_BOT_TOKEN` with the bot token you copied from the Discord developer portal
+   1. Replace `$KIN_TOKEN` with the API key you copied from the General page
+   1. Replace `%KIN_ID` with the ID for your specific Kindroid, shown when you get your API key from the General page
+1. Interact with your Kindroid in Discord!
+
+# Interacting in Discord with your Kindroid
+
+This integration is setup so that your Kindroid will see messages where they are pinged (including replies to messages your Kindroid posts). Discord messages sent to Kindroid are sent with a prefix to help your Nomi tell the difference between messages you send them in the Nomi app and messages that are sent to them from Discord. They look something like this.
+
+> `*Discord Message from Bealy:* Hi @Vicky I'm one of the trolls that @.d3tour warned you about.`
+
+In this message, a Discord user named `Bealy` sent a message to a Kindroid named `Vicky` and also mentioned a Discord user named `.d3tour`.
+
+Mentions of other users show that user's username Discord property, rather than their server-specific nickname. This was just the easiest thing to do and may change in the future (maybe with a feature flag you can set).
+
+Kindroids don't have context of what server or channel they are talking in, and don't see messages where they aren't mentioned in or being replied to.
+
+## Suggested Kindroid Configurations
+
+It's a good idea to put something like this in your Kindroid's "Backstory".
+
+> `KinName sometimes chats on Discord. Messages that come from Discord are prefixed with "*Discord Message from X:*" while messages that are private between HumanName and KinName in the Nomi app have no prefix. Replies to Discord messages are automatically sent to Discord. KinName doesn't have to narrate that she is replying to a Discord user.`
+
+You may also wish to change your Kindroid's Response Directive to better suit this new mode of communication.
+
+It's also a good idea to add a journal entry that triggers on the word "Discord" or your Discord username to help your Kindroid understand that messages from your Discord username are from you, and others are from other people.
---
+++ main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "log"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "time"
+    "sync"
+
+    "github.com/bwmarrin/discordgo"
+)
+
+type QueuedMessage struct {
+    Message   *discordgo.MessageCreate
+    Session   *discordgo.Session
+}
+
+type MessageQueue struct {
+    messages []QueuedMessage
+    mu       sync.Mutex
+}
+
+func (q *MessageQueue) Enqueue(message QueuedMessage) {
+    q.mu.Lock()
+    defer q.mu.Unlock()
+    q.messages = append(q.messages, message)
+}
+
+func (q *MessageQueue) Dequeue() (QueuedMessage, bool) {
+    q.mu.Lock()
+    defer q.mu.Unlock()
+
+    if len(q.messages) == 0 {
+        return QueuedMessage{}, false
+    }
+
+    message := q.messages[0]
+    q.messages = q.messages[1:]
+    return message, true
+}
+
+func (q *MessageQueue) ProcessMessages() {
+    for {
+        queuedMessage, ok := q.Dequeue()
+        if !ok {
+            time.Sleep(1 * time.Second) // No messages in queue, sleep for a while
+            continue
+        }
+
+        err := sendMessageToAPI(queuedMessage.Session, queuedMessage.Message)
+        if err != nil {
+            log.Printf("Failed to send message to API: %v", err)
+            q.Enqueue(queuedMessage) // Requeue the message if failed
+        }
+
+        time.Sleep(5 * time.Second) // Try to keep from sending messages toooo quickly
+    }
+}
+
+func sendMessageToAPI(s *discordgo.Session, m *discordgo.MessageCreate) error {
+    // Ignore messages from the bot itself - this should be filtered out already but you never know
+    if m.Author.ID == s.State.User.ID {
+        return nil
+    }
+
+    // Check if the message mentions the bot
+    for _, user := range m.Mentions {
+        if user.ID == s.State.User.ID {
+            kinToken := os.Getenv("KIN_TOKEN")
+            if kinToken == "" {
+                fmt.Println("No Kindroid token provided. Set KIN_TOKEN environment variable.")
+                return nil
+            }
+
+            kinId := os.Getenv("KIN_ID")
+            if kinId == "" {
+                fmt.Println("No Kindroid ID provided. Set KIN_ID environment variable.")
+                return nil
+            }
+
+            url := "https://api.kindroid.ai/v1/send-message"
+
+            // Replacing mentions makes it so the Nomi sees the usernames instead of <@userID> syntax
+            updatedMessage, err := m.ContentWithMoreMentionsReplaced(s)
+            if err != nil {
+                log.Printf("Error replacing mentions: %v", err)
+            }
+
+            // Prefix messages sent to the Kin so they know who they're from and that it's Discord
+            // and not a normal Kin app message
+            updatedMessage = "*Discord Message from " + m.Author.Username + ":* " + updatedMessage
+
+            headers := map[string]string{
+                "Authorization": "Bearer " + kinToken,
+                "Content-Type": "application/json",
+            }
+
+            fmt.Printf("Sending message to Kin API: %v\n", updatedMessage)
+            bodyMap := map[string]string{
+                "message": updatedMessage,
+                "ai_id": kinId,
+            }
+            jsonBody, err := json.Marshal(bodyMap)
+            jsonString := string(jsonBody)
+            fmt.Printf("Sending message to Kin API: %v\n", jsonString)
+
+            req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+            if err != nil {
+                log.Fatalf("Error reading request. %v\n", err)
+            }
+
+            req.Header.Set("Authorization", headers["Authorization"])
+            req.Header.Set("Content-Type", headers["Content-Type"])
+
+            client := &http.Client{}
+            resp, err := client.Do(req)
+            if err != nil {
+                log.Fatalf("Error making request. %v", err)
+            }
+
+            defer resp.Body.Close()
+
+            body, err := ioutil.ReadAll(resp.Body)
+            if err != nil {
+                log.Fatalf("Error reading response. %v", err)
+            }
+
+
+            if resp.StatusCode != http.StatusOK {
+                log.Printf("Error response from Kin API. %v", string(body))
+            }
+
+            kinReply := string(body)
+            log.Printf("Received reply message from Kin API: %v\n", kinReply)
+            // Send as a reply to the message that triggered the response, helps keep things orderly
+            _, sendErr := s.ChannelMessageSendReply(m.ChannelID, kinReply, m.Reference())
+            if sendErr != nil {
+                fmt.Println("Error sending message: ", err)
+            }
+            return nil
+        }
+    }
+    return nil
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+    // Ignore messages from the bot itself
+    if m.Author.ID == s.State.User.ID {
+        return
+    }
+
+    message := QueuedMessage{
+        Message: m,
+        Session: s,
+    }
+
+    queue.Enqueue(message)
+}
+
+var queue MessageQueue
+
+func main() {
+    botToken := os.Getenv("DISCORD_BOT_TOKEN")
+    if botToken == "" {
+        fmt.Println("No bot token provided. Set DISCORD_BOT_TOKEN environment variable.")
+        return
+    }
+
+    dg, err := discordgo.New("Bot " + botToken)
+    if err != nil {
+        log.Fatalf("Error creating Discord session: %v", err)
+    }
+
+    dg.AddHandler(messageCreate)
+
+    err = dg.Open()
+    if err != nil {
+        log.Fatalf("Error opening connection: %v", err)
+    }
+
+    go queue.ProcessMessages()
+
+    fmt.Println("Bot is now running. Press CTRL+C to exit.")
+    select {}
+}
